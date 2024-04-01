4df2bb1ded0077f84b773665845ab3f29f552546
KlnSdr <monsieurk1209@gmail.com>
2024-04-02 00:15:11 +0200

    load css using js

--- nidhogg/index.ts
+++ nidhogg/index.ts
@@ -1,5 +1,16 @@
 function startup() {
+  loadCss();
   addConsoleToDOM();
+}
+
+function loadCss() {
+  var link = document.createElement("link");
+  link.href = "style.css";
+  link.type = "text/css";
+  link.rel = "stylesheet";
+  link.media = "screen,print";
+
+  document.getElementsByTagName("head")[0].appendChild(link);
 }
 
 function newLogMessage(
