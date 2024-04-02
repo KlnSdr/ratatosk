eab8716b478abd9f98229f1c31265bd4eb631fe0
KlnSdr <monsieurk1209@gmail.com>
2024-04-02 11:52:22 +0200

    load socket.io using js

--- nidhogg/index.ts
+++ nidhogg/index.ts
@@ -1,6 +1,9 @@
 function startup() {
+  loadSocketIO();
   loadCss();
-  addConsoleToDOM();
+  setTimeout(() => {
+    addConsoleToDOM();
+  }, 3000);
 }
 
 function loadCss() {
@@ -11,6 +14,13 @@ function loadCss() {
   link.media = "screen,print";
 
   document.getElementsByTagName("head")[0].appendChild(link);
+}
+
+function loadSocketIO() {
+  var script = document.createElement("script"); // create a script DOM node
+  script.src = "/socket.io/socket.io.js"; // set its src to the provided URL
+
+  document.head.appendChild(script); // add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
 }
 
 function newLogMessage(
