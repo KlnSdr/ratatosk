45d330bf8d829495c4236ab55cf65c2b490b7178
KlnSdr <monsieurk1209@gmail.com>
2024-04-03 19:35:48 +0200

    replace express-static with the probably wonkiest implementation to inject nidhogg when sending html files

--- ratatosk/server.ts
+++ ratatosk/server.ts
@@ -1,8 +1,9 @@
-import express, { Application, request, response } from "express";
+import express, { Application, Request, Response } from "express";
 import { config as dotEnvconfig } from "dotenv";
 import { Logger } from "./util/Logger";
 import { Socket } from "socket.io";
 import http, { Server } from "http";
+import fs from "fs";
 
 interface obj {
   [key: string]: any;
@@ -19,16 +20,15 @@ dotEnvconfig();
 const LOGGER: Logger = new Logger("ratatosk.server");
 
 const PORT: number = parseInt(process.env.PORT || "3001");
-const TARGET: string | null = process.env.STATIC_DIR || null;
+const STATIC_DIR: string | null = process.env.STATIC_DIR || null;
 
 const app: Application = express();
 app.use(express.json());
 app.use(express.static("public"));
-if (TARGET == null) {
+if (STATIC_DIR == null) {
   LOGGER.error("no static dir given, abort");
   process.exit(1);
 }
-app.use(express.static(TARGET));
 app.use(express.urlencoded({ extended: true }));
 
 LOGGER.info("setting up websocket server...");
@@ -65,6 +65,44 @@ io.on("connection", (socket: Socket) => {
           LOGGER.error("UNKNOWN LOG TYPE: " + data.payload.type);
           break;
       }
+    }
+  });
+});
+
+app.get("*", (req: Request, res: Response) => {
+  let url: string = req.url;
+
+  if (url === "/") {
+    url = "/index.html";
+  }
+
+  const path: string = STATIC_DIR + url;
+  fs.readFile(path, { encoding: "utf-8" }, function (err, data) {
+    if (!err) {
+      const contentType: string = (() => {
+        if (url.endsWith("html")) {
+          return "text/html";
+        } else if (url.endsWith("css")) {
+          return "text/css";
+        } else if (url.endsWith("js")) {
+          return "text/javascript";
+        }
+        return "";
+      })();
+      res.writeHead(200, { "Content-Type": contentType });
+      if (url.endsWith("html")) {
+        res.write(
+          data.replace(
+            "<head>",
+            '<head><script src="/nidhogg/index.js"></script>'
+          )
+        );
+      } else {
+        res.write(data);
+      }
+      res.end();
+    } else {
+      res.sendStatus(404);
     }
   });
 });
